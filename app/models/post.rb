a1b6f509124331b659c41c4090130244e0d58bcf
vibhoo <mishrarock2010@gmail.com>
2014-05-25 23:39:10 +0530

    streching fixed

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,7 @@ class Post < ActiveRecord::Base
 
   belongs_to :user
 
-  has_attached_file :avatar, :styles => { :medium => "400x400>", :thumb => "150x150>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => {:original=> "" , :medium => "400x400>", :thumb => "150x150>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 end
