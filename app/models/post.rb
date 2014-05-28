f169d23501de3383ad2598e4bf23cd8b829fc29d
vibhoo <mishrarock2010@gmail.com>
2014-05-28 11:48:01 +0530

    missing image fixed

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,7 @@ class Post < ActiveRecord::Base
 
   belongs_to :user
 
-  has_attached_file :avatar, :styles => {:original=> "" , :medium => "400x400>", :thumb => "150x150>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => {:original=> "" , :medium => "400x400>", :thumb => "150x150>" }, :default_url => "/assets/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ActiveRecord::Base
   validates_length_of :username, minimum: 3, too_short: "Username must have atleast 2 characters"
 
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/assets/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 end
